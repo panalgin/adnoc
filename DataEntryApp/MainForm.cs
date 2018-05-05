1af80db999731321f30a57bdcbfff5876718e136
panalgin <mashadow@outlook.com>
2018-05-05 18:53:04 +0300

    a little more gui

--- DataEntryApp/MainForm.cs
+++ DataEntryApp/MainForm.cs
@@ -27,5 +27,58 @@ namespace DataEntryApp
                 var guests = db.Guests.ToList();
             }
         }
+
+        private void Search_Box_TextChanged(object sender, EventArgs e)
+        {
+            if (this.Search_Box.Text.Length > 0)
+            {
+                var key = this.Search_Box.Text.ToUpper();
+
+                using (var context = new EventEntities())
+                {
+                    var result = context.Guests.Where(q => (q.Name.ToUpper() + " " + q.Surname.ToUpper()).Contains(key)).ToList();
+
+                    if (result.Count > 0)
+                    {
+                        this.listView1.BeginUpdate();
+
+                        this.listView1.Items.Clear();
+
+                        result.All(delegate (Guest quest)
+                        {
+                            var item = new ListViewItem()
+                            {
+                                Text = quest.Prefix,
+                            };
+
+                            item.SubItems.Add(quest.Name);
+                            item.SubItems.Add(quest.Surname);
+                            item.SubItems.Add(quest.Role);
+                            item.SubItems.Add(quest.Organization);
+
+                            listView1.Items.Add(item);
+
+                            return true;
+                        });
+
+                        this.listView1.EndUpdate();
+
+
+                    }
+                    else
+                        this.listView1.Items.Clear();
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
