d0fa1df1e7e605c4580e73bf5ecf14bc43ff059b
panalgin <mashadow@outlook.com>
2018-05-05 13:10:54 +0300

    db model

---
+++ DataEntryApp/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity.Core.EntityClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEntryApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var db = new EventEntities())
+            {
+                var guests = db.Guests.ToList();
+            }
+        }
+    }
+}
--- DataEntryApp/Program.cs
+++ DataEntryApp/Program.cs
@@ -16,7 +16,7 @@ namespace DataEntryApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
